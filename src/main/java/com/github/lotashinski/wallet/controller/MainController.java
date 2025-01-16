c4006639b71794070b7baac83d7c0fe4a9ed7fbd
Alexander Lotashinsky <lat443152@gmail.com>
2025-01-16 16:38:39 +0300

    Create info messages for new users

--- src/main/java/com/github/lotashinski/wallet/controller/MainController.java
+++ src/main/java/com/github/lotashinski/wallet/controller/MainController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.github.lotashinski.wallet.service.CategoryServiceInterfate;
 import com.github.lotashinski.wallet.service.TransfersServiceInterface;
+import com.github.lotashinski.wallet.service.WalletServiceInterface;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,10 +16,16 @@ public class MainController {
 	
 	private final TransfersServiceInterface transferService;
 	
+	private final WalletServiceInterface walletService;
+	
+	private final CategoryServiceInterfate categoryService;
+	
 	
 	@GetMapping({"/", "/main"})
 	public String home(Model model) {
 		model.addAttribute("transfers", transferService.getLast());
+		model.addAttribute("categories", categoryService.getAll());
+		model.addAttribute("wallets", walletService.getAll());
 		
 		return "index";
 	}
